71f5651bfdf28bac92bc3ced1014f20fc04412b1
Данил Немцев <danilnemcev@MacBook-Pro-Danil.local>
2021-11-13 20:52:17 +0300

    Fixed the conflict

--- src/MyDrawPanel.java
+++ src/MyDrawPanel.java
@@ -7,12 +7,7 @@ public class MyDrawPanel extends JPanel{
         g.setColor(getRandomColor());
         g.fillRect(0,0,this.getWidth(), this.getHeight());
         g.setColor(getRandomColor());
-<<<<<<< HEAD
-        g.fillOval(50, 70, 100, 100);
-=======
-        g.fillOval(80, 70, 100, 100);
->>>>>>> 6a0502c0bfd062aea8fa014c2c1d927c4b95e66c
-    }
+        g.fillOval(70, 70, 100, 100);}
     public Color getRandomColor(){
         int red = (int) (Math.random() * 255);
         int green = (int) (Math.random() * 255);
