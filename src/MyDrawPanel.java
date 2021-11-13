0d57038071b716bc8a1ad406c1940d6dd533741f
Данил Немцев <danilnemcev@MacBook-Pro-Danil.local>
2021-11-13 20:13:11 +0300

    Add the random color setting for background

--- src/MyDrawPanel.java
+++ src/MyDrawPanel.java
@@ -4,12 +4,16 @@ import java.awt.*;
 import javax.swing.*;
 public class MyDrawPanel extends JPanel{
     public void paintComponent(Graphics g){
+        g.setColor(getRandomColor());
         g.fillRect(0,0,this.getWidth(), this.getHeight());
+        g.setColor(getRandomColor());
+        g.fillOval(70, 70, 100, 100);
+    }
+    public Color getRandomColor(){
         int red = (int) (Math.random() * 255);
         int green = (int) (Math.random() * 255);
         int blue = (int) (Math.random() * 255);
         Color randomColour = new Color(red, green, blue);
-        g.setColor(randomColour);
-        g.fillOval(70, 70, 100, 100);
+        return randomColour;
     }
 }
